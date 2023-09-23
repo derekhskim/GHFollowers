cc7f4564a7b8098ee427e44c1b69f28f09287037
[treasure3210] <treasure3210@gmail.com>
2023-09-22 20:37:44 -0600

    Add custom extension to call a method to present AlertVC

---
+++ GHFollowers/GHFollowers/UIViewController+.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+.swift
+//  GHFollowers
+//
+//  Created by Derek Kim on 2023-09-22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTtile: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+}
