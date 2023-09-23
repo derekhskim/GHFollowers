9b35413b39bf30b29c035bc626ff6874afbb2652
[treasure3210] <treasure3210@gmail.com>
2023-09-23 15:11:56 -0600

    Create raw value enum for handling error messages, refactoring NetworkManager

--- GHFollowers/GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Controllers/FollowerListVC.swift
@@ -20,7 +20,7 @@ class FollowerListVC: UIViewController {
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
             guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Oh No..!", message: errorMessage!, buttonTitle: "Okay")
+                self.presentGFAlertOnMainThread(title: "Oh No..!", message: errorMessage!.rawValue, buttonTitle: "Okay")
                 return
             }
             
