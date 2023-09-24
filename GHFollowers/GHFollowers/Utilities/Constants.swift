0242f7f695739cf2990c8d10c464b21da8ff3fc0
[treasure3210] <treasure3210@gmail.com>
2023-09-24 16:13:32 -0600

    Create UserInfVC and a childVC for header view display

---
+++ GHFollowers/GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Derek Kim on 2023-09-24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
