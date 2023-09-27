417c30d73ec1e58172b786be1ca171bbc977a66b
[treasure3210] <treasure3210@gmail.com>
2023-09-26 22:31:42 -0600

    Configure subclasses and integrate to UserInfoVC

---
+++ GHFollowers/GHFollowers/Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,25 @@
+//
+//  GFFollowerVC.swift
+//  GHFollowers
+//
+//  Created by Derek Kim on 2023-09-26.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    // MARK: - Function
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+
+}
---
+++ GHFollowers/GHFollowers/Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,25 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Derek Kim on 2023-09-26.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    // MARK: - Function
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "Github Profile")
+    }
+
+}
