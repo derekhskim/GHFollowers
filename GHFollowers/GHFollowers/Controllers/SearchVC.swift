05769cbc01a4bc63daf7e90dc992edf2c529384d
[treasure3210] <treasure3210@gmail.com>
2023-09-28 20:40:03 -0600

    Refactor VC flow and logic, textfield

--- GHFollowers/GHFollowers/Controllers/SearchVC.swift
+++ GHFollowers/GHFollowers/Controllers/SearchVC.swift
@@ -28,6 +28,7 @@ class SearchVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        usernameTextField.text = ""
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
@@ -43,9 +44,9 @@ class SearchVC: UIViewController {
             return
         }
         
-        let followerListVC = FollowerListVC()
-        followerListVC.username = usernameTextField.text
-        followerListVC.title = usernameTextField.text
+        usernameTextField.resignFirstResponder()
+        
+        let followerListVC = FollowerListVC(username: usernameTextField.text!)
         navigationController?.pushViewController(followerListVC, animated: true)
     }
     
